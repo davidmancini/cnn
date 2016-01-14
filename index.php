fa31b678cec9509b1a18a13de4b34c03caf101da
David Mancini <mancini.david@gmail.com>
2016-01-14 16:17:59 -0700

    Added persona data and more use case data.

--- index.php
+++ index.php
@@ -12,14 +12,30 @@
 		<!-- PERSONA -->
 		<div class="boxtitle">Persona</div>
 		<div class="box">
-			Box
+			<span class="boxheading">Demographics</span><br>
+				Name: Jacob Alexander Mayer<br>
+				Age: 37 years old<br>
+				Career: Manager of an advertising agency<br>
+				Education: BA in marketing<br>
+				Ethnicity: Caucasian<br>
+				Family Status: Married<br><br>
+			<span class="boxheading">Website Goals</span><br>
+				Jacob wants to quickly find and read relevant news and view associated media.<br>
+
+
 		</div><br>
 
 		<!-- USE CASES -->
 		<div class="boxtitle">Use Cases</div>
 		<div class="box">
-			Main Goal<br>
-			Steps
+			<span class="boxheading">Main Goal</span><br>
+				A visitor comes to the news article page to read a news story and view associated media.  The visitor should also be able to identify the author and the date and time that the article was published.<br><br>
+			<span class="boxheading">Steps</span>
+				<ol>
+					<li>The author must log into the system, so his authorId can be associated with the article.</li>
+					<li>The author adds copy and associates some media.  The articleId will be associated with the mediaId and authorId in the database.</li>
+					<li>Now that the article is published, a viewer can visit the site to read the article and view the media.</li>
+				</ol>
 		</div>
 
 	</body>
