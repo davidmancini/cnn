eaefa89f26e67aa023fb5e482ec70c0bf4fa8419
David Mancini <mancini.david@gmail.com>
2016-01-14 15:40:37 -0700

    Added div classes and structure for project.

--- index.php
+++ index.php
@@ -9,9 +9,18 @@
 	<body>
 		<h1>Data Design Project: CNN Article</h1>
 
+		<!-- PERSONA -->
 		<div class="boxtitle">Persona</div>
 		<div class="box">
 			Box
 		</div>
+
+		<!-- USE CASES -->
+		<div class="boxtitle">Use Cases</div>
+		<div class="box">
+			Main Goal<br>
+			Steps
+		</div>
+
 	</body>
 </html>
