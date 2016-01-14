7258220954fcf523d0da1689a5c4139c40841725
David Mancini <mancini.david@gmail.com>
2016-01-14 15:33:35 -0700

    Added CSS and Google Fonts

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>CNN Article Project</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link href='https://fonts.googleapis.com/css?family=News+Cycle|Raleway' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<h1>Data Design Project: CNN Article</h1>
+
+		<div class="boxtitle">Persona</div>
+		<div class="box">
+			Box
+		</div>
+	</body>
+</html>
