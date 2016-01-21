c944b9c3b6ad437cd654060602b21f1398b9c0ae
David Mancini <mancini.david@gmail.com>
2016-01-21 10:12:38 -0700

    Added additional accessors and mutators.

--- php/classes/author.php
+++ php/classes/author.php
@@ -13,24 +13,27 @@ class Author {
 	 * @var int $authorId
 	 */
 	private $authorId;
+
 	/*
 	 * email is the author's email address
 	 * @var string $email
 	 */
 	private $email;
+
 	/*
 	 * name is the author's first and last name
 	 * @var string $name
 	 */
 	private $name;
+
 	/*
 	 * title is the author's title (example: "Senior White House Correspondent")
 	 * @var string $title
 	 */
 	private $title;
+
 	/*
-	 * accessor method for author id
-	 *
+	 * Accessor method for author id
 	 * @return int value of author id
 	 */
 	public function getAuthorId() {
@@ -39,7 +42,6 @@ class Author {
 
 	/*
 	 * Mutator for author id
-	 *
 	 * @param int $newAuthorId new value of author id
 	 * @throws InvalidArgumentException if author id is not an integer
 	 * @throws RangeException if author id is negative
@@ -49,7 +51,7 @@ class Author {
 
 		//Exception if author id is not an integer
 		if($newAuthorId === false) {
-			throw (new InvalidArgumentException("author id is not an integer"));
+			throw (new TypeError("author id is not an integer"));
 		}
 
 		//Exception if author id is negative
@@ -57,7 +59,86 @@ class Author {
 			throw (new RangeException("author id must be positive"));
 		}
 
-		//if author id input passes the above tests (therefore, it is a positive integer), it is considered valid, so it is saved as the author id
+		//If author id input passes the above tests (therefore, it is a positive integer), it is considered valid, so it is saved as the author id
 		$this->authorId = $newAuthorId;
+	}
+
+	/*
+	 * Accessor method for email
+	 *
+	 * @return string value of email
+	 */
+	public function getEmail() {
+		return $this->email;
+	}
+
+	/*
+	 * Mutator for email
+	 *
+	 * @param string $newEmail new value of email
+	 * @throws InvalidArgumentException if email is not a valid email address
+	 */
+	public function setEmail($newEmail){
+		$newEmail = filter_var($newEmail, FILTER_VALIDATE_EMAIL);
+
+		//Exception if not a valid email address
+		if($newEmail === false) {
+			throw (new InvalidArgumentException("email is not a vaild email"));
+		}
+		//If input is a vaild email address, save the value
+		$this->email = $newEmail;
+	}
+
+	/*
+	 * Accessor method for name
+	 *
+	 * @return string for name
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/*
+	 * Mutator for name
+	 *
+	 * @param string $newName new value of name
+	 * @throws InvalidArgumentException if name is only non-sanitized sting data
+	 */
+	public function setName ($newName) {
+		$newName = filter_var($newName, FILTER_SANITIZE_STRING);
+
+		//Exception if input is only non-sanitized string data
+		if($newName === false) {
+			throw (new InvalidArgumentException("name is not a valid string"));
+		}
+
+		//If input is a valid string, save the value
+		$this->name = $newName;
+	}
+
+	/*
+	 * Accessor method for title
+	 *
+	 * @return string for name
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/*
+	 * Mutator method for title
+	 *
+	 * @throws InvalidArgumentException if title is only non-standardized string data
+	 */
+	public function setTitle($newTitle) {
+		$newTitle = filter_var($newTitle, FILTER_SANITIZE_STRING);
+
+		//Exception if input is only non-sanitized string data
+		if($newTitle === false) {
+			throw (new InvalidArgumentException("title is not a valid string"));
+		}
+
+		//If input is a vals string, save the value
+		$this->title = $newTitle;
 	}
 }
